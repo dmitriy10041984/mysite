22fae4e2ebe2d94313f44321f3d8b6976215c9f9
dmitriy10041984 <scorpion100484@mail.ru>
2016-08-28 14:35:49 +0300

    four

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import url, patterns, include
+from django.conf.urls import url
 from django.contrib import admin
-from django.conf import settings
+
 from . import views
 
 urlpatterns = [
